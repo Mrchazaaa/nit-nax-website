c534654b77d27e2ec1524cc134d6c28e0f540658
mrchazaaa <charlie.howlett@live.co.uk>
2019-10-16 23:48:35 +0100

    continued working on navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,37 +1,32 @@
 <template>
-<nav>
-  <v-card
-    color="primary"
-    flat
-    tile
-  >
-    <v-toolbar dense color="primary" class="white--text">
-      
+  <nav>
+    <v-card color="primary" flat tile>
+      <v-toolbar :height="100" dense color="primary" class="white--text d-flex justify-content-center">
 
-      <v-toolbar-title>NitNax</v-toolbar-title>
+        <v-toolbar-title><h1>NitNax</h1></v-toolbar-title>
 
-      <div class="flex-grow-1"></div>
+        <div class="flex-grow-1"></div>
 
-      <v-btn icon>
-        <v-icon color="white">mdi-magnify</v-icon>
-      </v-btn>
+        <v-toolbar-items class="hidden-sm-and-down">
+          <v-btn text>Home</v-btn>
 
-      <v-btn icon>
-        <v-icon color="white">mdi-heart</v-icon>
-      </v-btn>
+          <v-btn text>Shop</v-btn>
 
-      <v-btn icon>
-        <v-icon color="white">mdi-dots-vertical</v-icon>
-      </v-btn>
+          <v-btn text>Tutorials</v-btn>
 
-      <v-app-bar-nav-icon color="white" @click="drawer = !drawer"></v-app-bar-nav-icon>
+          <v-btn text>WorkShops</v-btn>
 
-    </v-toolbar>
-  </v-card>
-  <v-navigation-drawer v-model="drawer" app class="indigo">
-    <p>test</p>
-  </v-navigation-drawer>
-</nav>
+          <v-btn text>Contact</v-btn>
+        </v-toolbar-items>
+
+        <!-- <v-app-bar-nav-icon class="hidden-md-and-up" color="white" @click="drawer = !drawer"></v-app-bar-nav-icon> -->
+
+      </v-toolbar>
+    </v-card>
+    <!-- <v-navigation-drawer v-model="drawer" app class="indigo">
+      <p>test</p>
+    </v-navigation-drawer> -->
+  </nav>
 </template>
 
 <script>
@@ -40,10 +35,21 @@ export default {
   data() {
     return {
       drawer: false
-    }
+    };
   }
 };
 </script>
 
 <style>
+.v-btn {
+  color: white !important;
+}
+h1 {
+  /* margin: 10px 60px !important; */
+  font-size: 64px;
+}
+.v-toolbar{
+  /* height: 100px !important; */
+  padding: 0 60px;
+}
 </style>
